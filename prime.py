9d47615aa5bfaaee656d265480660de320308667
Sujaldeep51 <sinhasujal805@gmail.com>
2024-08-21 20:03:31 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -6,10 +6,12 @@ c=0
 for i in range(1,n+1):
     if(n%i==0):
         c=c+1
-if(c==2):
-    st.success("It is a prime number")
-else:
-    st.error("It is not a prime number") 
+if st.button("Check"):
+    if(c==2):
+        st.success("Prime")
+    else:
+        st.error("Not Prime")
+
 
            
     
