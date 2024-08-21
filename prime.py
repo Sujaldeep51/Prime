e5a999a22766c0a1149f267ab8e4b8418db70a5c
Sujaldeep51 <sinhasujal805@gmail.com>
2024-08-21 19:45:56 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,6 +1,6 @@
 import streamlit as st 
-n=st.text_input("Enter the number to check")
-n=int(n)
+n=st.number_input("Enter the number to check")
+#n=int(n)
 c=0
 for i in range(1,n+1):
     if(n%i==0):
